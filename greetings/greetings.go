4bef91868bd752fa43767659360bf8b64cc028b5
ummesalma303 <ummesalma333999@gmail.com>
2025-06-10 20:26:47 +0600

    remove unnecessary commit

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,9 +1,18 @@
 package greetings
 
-import "fmt"
+import (
+    "errors"
+    "fmt"
+)
 
 
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+    // If no name was given, return an error with a message.
+    if name == "" {
+        return "", errors.New("name not found 🧐")
+    }
+
+
     message := fmt.Sprintf("Hi, %v. Welcome!", name)
-    return message
+    return message, nil
 }
