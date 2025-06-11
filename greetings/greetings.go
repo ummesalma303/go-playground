dda784d452767a93c436f566709912f9b26b2178
ummesalma303 <ummesalma333999@gmail.com>
2025-06-11 13:49:54 +0600

    added random text genaretor function

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,6 +3,7 @@ package greetings
 import (
     "errors"
     "fmt"
+    "math/rand"
 )
 
 
@@ -13,6 +14,23 @@ func Hello(name string) (string, error) {
     }
 
 
-    message := fmt.Sprintf("Hi, %v. Welcome!", name)
+   // message := fmt.Sprintf("Hi, %v. Welcome!", name)
+    message := fmt.Sprintf(randomFormat(), name)
     return message, nil
+}
+
+
+func randomFormat() string {
+    // A slice of message formats.
+    formats := []string{
+        "Hi, %v!🐎. Welcome!",
+        "Great to see you, %v! 🦙",
+        "Hail, %v!🐴. Well met!",
+        "Life is a %v!🦋",
+        "Everything is %v!.🥚🥚🥚",
+    }
+
+    // Return a randomly selected message format by specifying
+    // a random index for the slice of formats.
+    return formats[rand.Intn(len(formats))]
 }
--- hello/hello.go
+++ hello/hello.go
@@ -3,18 +3,16 @@ package main
 import (
     "fmt"
     "log"
-
     "example.com/greetings"
 )
 
 func main() {
     log.SetPrefix("greetings: ")
-//    log.SetFlags(0)
+    log.SetFlags(0)
 
     // Request a greeting message.
-    message, err := greetings.Hello("")
-    // If an error was returned, print it to the console and
-    // exit the program.
+    message,err:= greetings.Hello("ghorar dim")
+
     if err != nil {
         log.Fatal(err)
     }
