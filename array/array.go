11d9baf2f57e8cac18990d1a6e2d9f048061df7c
ummesalma303 <ummesalma333999@gmail.com>
2025-06-11 23:30:58 +0600

    added array

---
+++ array/array.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello")
+
+    //  int array 
+    var a [4]int
+    a[2] = 1
+    i := a[2]
+    fmt.Println(a, i)
+    // string array
+    b := [5]string{"Penn", "Teller"}
+    fmt.Println(b)
+    // slice array
+    letters := []string{"a", "b", "c", "d"}
+    fmt.Println(letters)
+
+}
