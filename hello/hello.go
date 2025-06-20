01a086d0c677e287e320cf2d5951d3af439207d2
ummesalma303 <ummesalma333999@gmail.com>
2025-06-20 22:55:49 +0600

    remove unnecessary comment

--- hello/hello.go
+++ hello/hello.go
@@ -6,30 +6,6 @@ import (
     "example.com/greetings"
 )
 
-/*
-func main() {
-    log.SetPrefix("greetings: ")
-    log.SetFlags(0)
-
-// A slice of names.
-    names := []string{"Gladys", "Samantha", "Darrin"}
-
-
-    // Request a greeting message.
-  message,err:= greetings.Hello(names)
-
-
-
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    // If no error was returned, print the returned message
-    // to the console.
-    fmt.Println(message)
-}
-*/
-
 
 func main() {
     // Set properties of the predefined Logger, including
